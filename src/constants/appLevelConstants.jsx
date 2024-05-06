617d259711cb5f2c31015ade2befce4b2c9bc4e9
AnamAnsari <anama2379@gmail.com>
2024-05-07 00:23:17 +0530

    update url

--- src/constants/appLevelConstants.jsx
+++ src/constants/appLevelConstants.jsx
@@ -1,5 +1,7 @@
 export  const  apis = {
-    BASE_URL: 'http://127.0.0.1:8001',
+    // BASE_URL: 'http://127.0.0.1:8001',
+    BASE_URL: 'https://syndx-backend.onrender.com',
+
     API_LOGIN_GET_TOKEN: "/auth/login",
     API_USER_ACCOUNT_CREATION: "/auth",
 }
